9daf08b3692fa6766f1dcc7e6d1ab1da8a18c522
ivan <iv.morenoma@gmail.com>
2024-02-02 13:19:01 +0100

    define the url to index.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from myWeb.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #path('', include('admin_material.urls')),
     path('admin/', admin.site.urls),
+    path('', index, name='index')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
