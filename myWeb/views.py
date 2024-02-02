f8b0d6322314f62935bf7374ef6d6bf562f089ec
ivan <iv.morenoma@gmail.com>
2024-02-02 19:04:33 +0100

    login function

--- myWeb/views.py
+++ myWeb/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User, Register, Courses
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 # from django.shortcuts import HttpResponseRedirect
 
 # Create your views here.
@@ -7,12 +9,29 @@ from .models import User, Register, Courses
 def index(request):
     return render(request, 'myWeb/index.html')
 
+def loggin_form(request):
+    if  request.method == "POST":
+        email = request.POST['InputEmail']
+        password = request.POST["InputPassword1"]
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('landing')
+        else:
+            error_message = "Invalid email or password"
+    else:
+        error_message = ''
+    return render(request, 'myWeb/index.html', {'error_message': error_message})
+
+
+def landing_page(request):
+    return render(request, 'myWeb/landing.html')
+
 def register(request):
     return render(request, 'myWeb/register.html')
 
 def cancel(request):
     return render(request, 'myWeb/index.html')
-
 
 def create_user(request):
     if  request.method == "POST":
