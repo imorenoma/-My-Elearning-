c9d0b9e3329187049a2186f275491311542f4c4d
ivan <iv.morenoma@gmail.com>
2024-02-02 14:27:09 +0100

    def to create user bbdd

--- myWeb/views.py
+++ myWeb/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import User, Register, Courses
 
 # Create your views here.
 
 def index(request):
     return render(request, 'myWeb/index.html')
+
+def create_user(request):
+    if  request.method == "POST":
+        register_id = request.POST.get('register_id')
+        course_id = request.POST.get('course_id')
+        
+        register =Register.objects.get(id=register_id)
+        course = Courses.objects.get(id=course_id)
+
+        user = User(register=register, course=course)
+        user.save()
+
+        return redirect('index')
+    return render(request, "myWeb/Register.html")
